2e19023338f6f8069018e61eb87362cd377869b0
NaloK7 <129076162+NaloK7@users.noreply.github.com>
2024-11-15 16:33:54 +0100

    nav fix

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ window.onload = function() {
     const nav = document.querySelector("nav");
     nav.innerHTML = `
         <a href="../index.html">home</a>
-        <a href="../simplecube/simpleCube.html">simple cube</a>
-        <a href="../oneface/oneFace.html">one face</a>
-        <a href="../cubeV1/cubeV1.html">cube V1</a>
-        <a href="../cubeV2/cubeV2.html">cube V2</a>
+        <a href="/simplecube/simpleCube.html">simple cube</a>
+        <a href="/oneface/oneFace.html">one face</a>
+        <a href="/cubeV1/cubeV1.html">cube V1</a>
+        <a href="/cubeV2/cubeV2.html">cube V2</a>
     `;
 };
 let rotateX = -20;
