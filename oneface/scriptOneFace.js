801e98100e309fccae1865b8b004c9b16c139689
NaloK7 <129076162+NaloK7@users.noreply.github.com>
2024-11-28 12:35:13 +0100

    reset position after rotation

--- oneface/scriptOneFace.js
+++ oneface/scriptOneFace.js
@@ -1,164 +1,63 @@
-let cube = [
+const refCube = [
   [
-    ["Ftl", "Ftc", "Ftr"],
-    ["Fml", "Fmc", "Fmr"],
-    ["Fbl", "Fbc", "Fbr"],
+    ["Ftl L F U", "Ftc M F U", "Ftr R F U"],
+    ["Fml L F E", "Fmc M F E", "Fmr R F E"],
+    ["Fbl L F D", "Fbc M F D", "Fbr R F D"],
   ],
   [
-    ["Ltl", "Ltc", "Ltr"],
-    ["Lml", "Lmc", "Lmr"],
-    ["Lbl", "Lbc", "Lbr"],
+    ["Ltl L U B", "Ltc L U S", "Ltr L F U"],
+    ["Lml L E B", "Lmc L E S", "Lmr L F E"],
+    ["Lbl L D B", "Lbc L D S", "Lbr L F D"],
   ],
   [
-    ["Btl", "Btc", "Btr"],
-    ["Bml", "Bmc", "Bmr"],
-    ["Bbl", "Bbc", "Bbr"],
+    ["Btl R U B", "Btc M U B", "Btr L U B"],
+    ["Bml R E B", "Bmc M E B", "Bmr L E B"],
+    ["Bbl R D B", "Bbc M D B", "Bbr L D B"],
   ],
   [
-    ["Rtl", "Rtc", "Rtr"],
-    ["Rml", "Rmc", "Rmr"],
-    ["Rbl", "Rbc", "Rbr"],
+    ["Rtl R F U", "Rtc R U S", "Rtr R U B"],
+    ["Rml R F E", "Rmc R E S", "Rmr R E B"],
+    ["Rbl R F D", "Rbc R D S", "Rbr R D B"],
   ],
   [
-    ["Utl", "Utc", "Utr"],
-    ["Uml", "Umc", "Umr"],
-    ["Ubl", "Ubc", "Ubr"],
+    ["Utl L U B", "Utc M U B", "Utr R U B"],
+    ["Uml L U S", "Umc M U S", "Umr R U S"],
+    ["Ubl L U F", "Ubc F M U", "Ubr F R U"],
   ],
   [
-    ["Dtl", "Dtc", "Dtr"],
-    ["Dml", "Dmc", "Dmr"],
-    ["Dbl", "Dbc", "Dbr"],
+    ["Dtl L F D", "Dtc M F D", "Dtr R F D"],
+    ["Dml L D S", "Dmc M D S", "Dmr R D S"],
+    ["Dbl L D B", "Dbc M D B", "Dbr R D B"],
   ],
 ];
 
-let refCube = [
-  [
-    [
-      ["Ftl", "LFU"],
-      ["Ftc", "MFU"],
-      ["Ftr", "RFU"],
-    ],
-    [
-      ["Fml", "LFE"],
-      ["Fmc", "MFE"],
-      ["Fmr", "RFE"],
-    ],
-    [
-      ["Fbl", "LFD"],
-      ["Fbc", "MFD"],
-      ["Fbr", "RFD"],
-    ],
-  ],
-  [
-    [
-      ["Ltl", "LUB"],
-      ["Ltc", "LUS"],
-      ["Ltr", "LFU"],
-    ],
-    [
-      ["Lml", "LEB"],
-      ["Lmc", "LES"],
-      ["Lmr", "LFE"],
-    ],
-    [
-      ["Lbl", "LDB"],
-      ["Lbc", "LDS"],
-      ["Lbr", "LFD"],
-    ],
-  ],
-  [
-    [
-      ["Btl", "RUB"],
-      ["Btc", "MUB"],
-      ["Btr", "LUB"],
-    ],
-    [
-      ["Bml", "REB"],
-      ["Bmc", "MEB"],
-      ["Bmr", "LEB"],
-    ],
-    [
-      ["Bbl", "RDB"],
-      ["Bbc", "MDB"],
-      ["Bbr", "LDB"],
-    ],
-  ],
-  [
-    [
-      ["Rtl", "RFU"],
-      ["Rtc", "RUS"],
-      ["Rtr", "RUB"],
-    ],
-    [
-      ["Rml", "RFE"],
-      ["Rmc", "RES"],
-      ["Rmr", "REB"],
-    ],
-    [
-      ["Rbl", "RFD"],
-      ["Rbc", "RDS"],
-      ["Rbr", "RDB"],
-    ],
-  ],
-  [
-    [
-      ["Utl", "LUB"],
-      ["Utc", "MUB"],
-      ["Utr", "RUB"],
-    ],
-    [
-      ["Uml", "LUS"],
-      ["Umc", "MUS"],
-      ["Umr", "RUS"],
-    ],
-    [
-      ["Ubl", "LUF"],
-      ["Ubc", "FMU"],
-      ["Ubr", "FRU"],
-    ],
-  ],
-  [
-    [
-      ["Dtl", "LFD"],
-      ["Dtc", "MFD"],
-      ["Dtr", "RFD"],
-    ],
-    [
-      ["Dml", "LDS"],
-      ["Dmc", "MDS"],
-      ["Dmr", "RDS"],
-    ],
-    [
-      ["Dbl", "LDB"],
-      ["Dbc", "MDB"],
-      ["Dbr", "RDB"],
-    ],
-  ],
-];
+// create a deep copy (modify cube doesn't modify refCube)
+// const cube = refCube.slice(); // DID NOT WORK
+const cube = JSON.parse(JSON.stringify(refCube));
 
 window.addEventListener(
   "DOMContentLoaded",
   () => {
     // displayMouseCoord();
     // setInterval(displayCubeTransform, 100);
-    generateCubeHTML(cube);
-    getSquare();
-    let allSquare = document.querySelectorAll(".square");
-    let displayTag = document.querySelector("#tag");
-    allSquare.forEach((square) => {
-      // setMovePossible(square);
 
-      //  add event to get and display coordinate of clicked square
-      //  (probable futur use to detect relative movement)
-      square.addEventListener("mousedown", () => {
-        let rect = square.getBoundingClientRect();
-        let face = square.classList[1];
-        displayTag.innerHTML = `${face}</br>squareX: ${rect.left.toFixed(
-          2
-        )}</br>squareY: ${rect.top.toFixed(2)}`;
-      });
-      square.style.transition = "rotate ease 0.5s";
-    });
+    generateCubeHTML(cube);
+    // let allSquare = document.querySelectorAll(".square");
+    // let displayTag = document.querySelector("#tag");
+    // allSquare.forEach((square) => {
+    //   // setMovePossible(square);
+
+    //   //  add event to get and display coordinate of clicked square
+    //   //  (probable futur use to detect relative movement)
+    //   square.addEventListener("mousedown", () => {
+    //     let rect = square.getBoundingClientRect();
+    //     let face = square.classList[1];
+    //     displayTag.innerHTML = `${face}</br>squareX: ${rect.left.toFixed(
+    //       2
+    //     )}</br>squareY: ${rect.top.toFixed(2)}`;
+    //   });
+    //   square.style.transition = "rotate ease 0.5s";
+    // });
   },
   false
 );
@@ -177,6 +76,229 @@ function updateDisplay(event) {
   pageX.innerText = event.pageX;
   pageY.innerText = event.pageY;
 }
+
+function generateCubeHTML(cube) {
+  const faceColors = {
+    front: "blue",
+    up: "white",
+    left: "red",
+    right: "orange",
+    back: "green",
+    bottom: "yellow",
+  };
+  // same order than cube
+  const faceNames = ["front", "left", "back", "right", "up", "bottom"];
+  const cubeContainer = document.querySelector(".cube");
+
+  faceNames.forEach((face, faceIndex) => {
+    cube[faceIndex].forEach((row, rowIndex) => {
+      row.forEach((element, columnIndex) => {
+        const div = document.createElement("div");
+        div.className = `square ${cube[faceIndex][rowIndex][columnIndex]}`;
+
+        const span = document.createElement("span");
+        span.className = faceColors[face];
+        span.textContent = `${cube[faceIndex][rowIndex][columnIndex]}`;
+
+        div.appendChild(span);
+        cubeContainer.appendChild(div);
+      });
+    });
+  });
+}
+
+// prevent mess up animation
+let isAnimate = false;
+
+function rotateGroupe(move, reverse = false) {
+  // 1. visual rotation
+  let axis = "";
+  if (!isAnimate) {
+    isAnimate = true;
+    let deg = "-";
+    if (reverse) {
+      deg = "";
+    }
+    if ((move == "L") | (move == "M") | (move == "R")) {
+      deg += "x";
+    } else if ((move == "U") | (move == "E") | (move == "D")) {
+      deg += "y";
+    } else if ((move == "F") | (move == "S") | (move == "B")) {
+      deg += "z";
+    }
+
+    let group = document.querySelectorAll(`.${move}`);
+    group.forEach((square) => {
+      square.style.transition = "rotate ease 0.5s";
+      square.classList.add(deg);
+    });
+
+    // 2. do the corresponding rotation in the cube matrice
+    switch (move) {
+      case "F":
+        moveF(cube);
+        break;
+
+      case "U":
+        moveU(cube);
+        break;
+
+      default:
+        console.log("function not ready");
+        break;
+    }
+
+    // 3. wait the end of animation and reassign square position using refCube
+    setTimeout(() => {
+      setNewPos();
+      isAnimate = false;
+    }, 500);
+  }
+}
+
+
+
+function setNewPos() {
+  // for each face
+  for (let face = 0; face < cube.length; face++) {
+    // for each row
+    for (let row = 0; row < cube[face].length; row++) {
+      // for each column
+      for (let column = 0; column < cube[face][row].length; column++) {
+        // get reference
+        let refSquare = refCube[face][row][column].split(" ");
+
+        // get the element name
+        let movedSquare = cube[face][row][column].split(" ");
+
+        // select the corresponding html element
+        let squareList = document.querySelectorAll(`.${movedSquare[0]}`);
+        squareList.forEach((square) => {
+          if (square.classList[0] != "temp") {
+            square.style.transition = "";
+            // remove old movement classes
+            let baseClass = `temp square `;
+            square.classList = baseClass;
+            square.children[0].innerText = refCube[face][row][column];
+            // add new movement classes
+            for (let j = 0; j < refSquare.length; j++) {
+              square.classList.add(refSquare[j]);
+            }
+          }
+        });
+      }
+    }
+  }
+  let clean = document.querySelectorAll(".square");
+  clean.forEach((element) => {
+    element.classList.remove("temp");
+  });
+}
+
+function rotateFace(face) {
+  const temp = [
+    [face[2][0], face[1][0], face[0][0]], // First column becomes first row (reversed)
+    [face[2][1], face[1][1], face[0][1]], // Second column becomes second row (reversed)
+    [face[2][2], face[1][2], face[0][2]], // Third column becomes third row (reversed)
+  ];
+  face[0] = temp[0];
+  face[1] = temp[1];
+  face[2] = temp[2];
+}
+
+function moveF(cube) {
+  // Rotate the front face
+  rotateFace(cube[0]);
+  // Save edges
+  const topEdge = cube[4][2].slice(); // UP last row
+  const rightEdge = cube[3].map((row) => row[0]);
+  const bottomEdge = cube[5][0].slice();
+  const leftEdge = cube[1].map((row) => row[2]);
+
+  // Update edges
+  cube[4][2] = leftEdge.reverse();
+  for (let i = 0; i < 3; i++) cube[3][i][0] = topEdge[i];
+  cube[5][0] = rightEdge.reverse();
+  for (let i = 0; i < 3; i++) cube[1][i][2] = bottomEdge[i];
+}
+
+function moveU(cube) {
+  // Rotate the top face
+  rotateFace(cube[4]);
+
+  // Save edges
+  const frontEdge = cube[0][0].slice();
+  const rightEdge = cube[3][0].slice();
+  const backEdge = cube[2][0].slice();
+  const leftEdge = cube[1][0].slice();
+
+  // Update edges
+  cube[0][0] = rightEdge;
+  cube[3][0] = backEdge;
+  cube[2][0] = leftEdge;
+  cube[1][0] = frontEdge;
+}
+
+//
+//
+// NOT USED FOR NOW
+//
+//
+
+// Update the classes of elements affected by the rotation
+function updateClasses(face, cube) {
+  // Loop through the rotated face
+  for (let row = 0; row < cube[face].length; row++) {
+    for (let col = 0; col < cube[face][row].length; col++) {
+      const element = cube[face][row][col]; // Get the element name
+      let square = document.querySelector(`.${element}`); // Select corresponding HTML element
+
+      if (square) {
+        // Update classes
+        let baseClass = `${square.classList[0]} ${square.classList[1]}`;
+        square.classList = baseClass;
+
+        // Add new movement classes
+        const movements = moveList[face][row][col];
+        for (let j = 0; j < moveList[face][row][col].length; j++) {
+          square.classList.add(moveList[face][row][col][j]);
+        }
+      }
+    }
+  }
+}
+const moveList = [
+  [
+    ["LFU", "MFU", "RFU"],
+    ["LFE", "MFE", "RFE"],
+    ["LFD", "MFD", "RFD"],
+  ], // FRONT
+  [
+    ["LUB", "LUS", "LFU"],
+    ["LEB", "LES", "LFE"],
+    ["LDB", "LDS", "LFD"],
+  ], // LEFT
+  [
+    ["RUB", "MUB", "LUB"],
+    ["REB", "MEB", "LEB"],
+    ["RDB", "MDB", "LDB"],
+  ], // BACK
+  [
+    ["RFU", "RUS", "RUB"],
+    ["RFE", "RES", "REB"],
+    ["RFD", "RDS", "RDB"],
+  ], // RIGHT
+  [
+    ["LUB", "MUB", "RUB"],
+    ["LUS", "MUS", "RUS"],
+    ["LUF", "FMU", "FRU"],
+  ], // UP
+  [
+    ["LFD", "MFD", "RFD"],
+    ["LDS", "MDS", "RDS"],
+    ["LDB", "MDB", "RDB"],
+  ], // DOWN
+];
 
 // display cube rotation to set relative face
 function displayCubeTransform() {
@@ -224,281 +346,6 @@ function getTransform(element) {
   let tZ = values[14];
 
   return { rX: rX, rY: rY, rZ: rZ, tX: tX, tY: tY, tZ: tZ };
-}
-
-// prevent mess up animation
-let isAnimate = false;
-
-function rotateGroupe(move, reverse = false) {
-  let axis = "";
-  if (!isAnimate) {
-    isAnimate = true;
-    let deg = -90;
-    if (reverse) {
-      deg = 90;
-    }
-    if ((move == "L") | (move == "M") | (move == "R")) {
-      axis = "x";
-    } else if ((move == "U") | (move == "E") | (move == "D")) {
-      axis = "y";
-    } else if ((move == "F") | (move == "S") | (move == "B")) {
-      axis = "z";
-    }
-
-    let group = document.querySelectorAll(`.${move}`);
-    group.forEach((square) => {
-      let angle = window.getComputedStyle(square).rotate;
-      if (angle != "none") {
-        angle = parseInt(angle.match(/[-]?\d+/g).join(""));
-      } else {
-        angle = 0;
-      }
-      let newAngle = angle + deg;
-      square.style.rotate = `${axis} ${newAngle}deg`;
-    });
-
-    switch (move) {
-      case "F":
-        moveF(cube);
-        break;
-      case "U":
-        moveU(cube);
-        break;
-
-      default:
-        console.log("function not ready");
-
-        break;
-    }
-    // getSquare();
-
-    setTimeout(() => {
-      isAnimate = false;
-    }, 500);
-  }
-}
-
-function generateCubeHTML(cube) {
-  const faceColors = {
-    front: "blue",
-    up: "white",
-    left: "red",
-    right: "orange",
-    back: "green",
-    bottom: "yellow",
-  };
-  // same order than cube
-  const faceNames = ["front", "left", "back", "right", "up", "bottom"];
-  const cubeContainer = document.querySelector(".cube");
-
-  faceNames.forEach((face, faceIndex) => {
-    cube[faceIndex].forEach((row, rowIndex) => {
-      row.forEach((element, columnIndex) => {
-        const div = document.createElement("div");
-        div.className = `square ${cube[faceIndex][rowIndex][columnIndex]}`;
-
-        const span = document.createElement("span");
-        span.className = faceColors[face];
-        span.textContent = `${cube[faceIndex][rowIndex][columnIndex]}`;
-
-        div.appendChild(span);
-        cubeContainer.appendChild(div);
-      });
-    });
-  });
-}
-
-function getSquare() {
-  let moveList = [
-    [
-      ["LFU", "MFU", "RFU"],
-      ["LFE", "MFE", "RFE"],
-      ["LFD", "MFD", "RFD"],
-    ], // FRONT
-    [
-      ["LUB", "LUS", "LFU"],
-      ["LEB", "LES", "LFE"],
-      ["LDB", "LDS", "LFD"],
-    ], // LEFT
-    [
-      ["RUB", "MUB", "LUB"],
-      ["REB", "MEB", "LEB"],
-      ["RDB", "MDB", "LDB"],
-    ], // BACK
-    [
-      ["RFU", "RUS", "RUB"],
-      ["RFE", "RES", "REB"],
-      ["RFD", "RDS", "RDB"],
-    ], // RIGHT
-    [
-      ["LUB", "MUB", "RUB"],
-      ["LUS", "MUS", "RUS"],
-      ["LUF", "FMU", "FRU"],
-    ], // UP
-    [
-      ["LFD", "MFD", "RFD"],
-      ["LDS", "MDS", "RDS"],
-      ["LDB", "MDB", "RDB"],
-    ], // DOWN
-  ];
-  // for each face
-  for (let face = 0; face < cube.length; face++) {
-    // for each row
-    for (let row = 0; row < cube[face].length; row++) {
-      // for each column
-      for (let column = 0; column < cube[face][row].length; column++) {
-        // get the element name
-        const element = cube[face][row][column];
-        // select the corresponding html element
-        let square = document.querySelector(`.${element}`);
-        // remove old movement classes
-        let baseClass = `${square.classList[0]} ${square.classList[1]}`;
-        square.classList = baseClass;
-        // add new movement classes
-        for (let j = 0; j < moveList[face][row][column].length; j++) {
-          square.classList.add(moveList[face][row][column][j]);
-        }
-      }
-    }
-  }
-}
-const moveList = [
-  [
-    ["LFU", "MFU", "RFU"],
-    ["LFE", "MFE", "RFE"],
-    ["LFD", "MFD", "RFD"],
-  ], // FRONT
-  [
-    ["LUB", "LUS", "LFU"],
-    ["LEB", "LES", "LFE"],
-    ["LDB", "LDS", "LFD"],
-  ], // LEFT
-  [
-    ["RUB", "MUB", "LUB"],
-    ["REB", "MEB", "LEB"],
-    ["RDB", "MDB", "LDB"],
-  ], // BACK
-  [
-    ["RFU", "RUS", "RUB"],
-    ["RFE", "RES", "REB"],
-    ["RFD", "RDS", "RDB"],
-  ], // RIGHT
-  [
-    ["LUB", "MUB", "RUB"],
-    ["LUS", "MUS", "RUS"],
-    ["LUF", "FMU", "FRU"],
-  ], // UP
-  [
-    ["LFD", "MFD", "RFD"],
-    ["LDS", "MDS", "RDS"],
-    ["LDB", "MDB", "RDB"],
-  ], // DOWN
-];
-
-
-// let refCube = [];
-// for (let face = 0; face < cube.length; face++) {
-//   let newFace = [];
-//   for (let row = 0; row < cube[face].length; row++) {
-//     let newRow = [];
-//     for (let col = 0; col < cube[face][row].length; col++) {
-//       newRow.push([cube[face][row][col], moveList[face][row][col]]);
-//     }
-//     newFace.push(newRow);
-//   }
-//   refCube.push(newFace);
-// }
-// console.log("🚀 ~ refCube:", refCube);
-
-// Update the classes of elements affected by the rotation
-function updateClasses(face, cube) {
-  // Loop through the rotated face
-  for (let row = 0; row < cube[face].length; row++) {
-    for (let col = 0; col < cube[face][row].length; col++) {
-      const element = cube[face][row][col]; // Get the element name
-      let square = document.querySelector(`.${element}`); // Select corresponding HTML element
-
-      if (square) {
-        // Update classes
-        let baseClass = `${square.classList[0]} ${square.classList[1]}`;
-        square.classList = baseClass;
-
-        // Add new movement classes
-        const movements = moveList[face][row][col];
-        for (let j = 0; j < moveList[face][row][col].length; j++) {
-          square.classList.add(moveList[face][row][col][j]);
-        }
-      }
-    }
-  }
-}
-
-// Update the transform property of affected elements
-function updateTransforms(face, cube) {
-  // Define new transforms for the rotated face
-  const newTransforms = [
-    ["rotateX(90deg)", "rotateX(90deg)", "rotateX(90deg)"],
-    ["rotateX(90deg)", "rotateX(90deg)", "rotateX(90deg)"],
-    ["rotateX(90deg)", "rotateX(90deg)", "rotateX(90deg)"],
-  ]; // Example: adjust this based on your specific rotation logic
-
-  // Loop through the rotated face
-  for (let row = 0; row < cube[face].length; row++) {
-    for (let col = 0; col < cube[face][row].length; col++) {
-      const element = cube[face][row][col]; // Get the element name
-      let square = document.querySelector(`.${element}`); // Select corresponding HTML element
-
-      if (square) {
-        // Update transform property
-        square.style.transform = newTransforms[row][col];
-      }
-    }
-  }
-}
-function rotateFace(face) {
-  // todo switch also transform value
-  const temp = [
-    [face[2][0], face[1][0], face[0][0]], // First column becomes first row (reversed)
-    [face[2][1], face[1][1], face[0][1]], // Second column becomes second row (reversed)
-    [face[2][2], face[1][2], face[0][2]], // Third column becomes third row (reversed)
-  ];
-  face[0] = temp[0];
-  face[1] = temp[1];
-  face[2] = temp[2];
-}
-
-function moveF(cube) {
-  // Rotate the front face
-  rotateFace(cube[0]);
-  updateClasses(0, cube);
-  // Save edges
-  const topEdge = cube[4][2].slice(); // UP last row
-  const rightEdge = cube[3].map((row) => row[0]);
-  const bottomEdge = cube[5][0].slice();
-  const leftEdge = cube[1].map((row) => row[2]);
-
-  // Update edges
-  cube[4][2] = leftEdge.reverse();
-  for (let i = 0; i < 3; i++) cube[3][i][0] = topEdge[i];
-  cube[5][0] = rightEdge.reverse();
-  for (let i = 0; i < 3; i++) cube[1][i][2] = bottomEdge[i];
-}
-
-function moveU(cube) {
-  // Rotate the top face
-  rotateFace(cube[4]);
-
-  // Save edges
-  const frontEdge = cube[0][0].slice();
-  const rightEdge = cube[3][0].slice();
-  const backEdge = cube[2][0].slice();
-  const leftEdge = cube[1][0].slice();
-
-  // Update edges
-  cube[0][0] = rightEdge;
-  cube[3][0] = backEdge;
-  cube[2][0] = leftEdge;
-  cube[1][0] = frontEdge;
 }
 
 // /**
