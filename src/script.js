29c204d83669ae73815947fa43d24d4b0242dab5
NaloK7 <129076162+NaloK7@users.noreply.github.com>
2024-12-11 17:23:14 +0100

    convert to object + fix(Cube.js): function mix + fix(cubeEventHandler.js): function handleCubeMovement

---
+++ src/script.js
@@ -0,0 +1,23 @@
+import { Cube } from "/src/Cube.js";
+import { CubeEventHandler } from "/src/CubeEventHandler.js";
+
+export const cubeEvent = new CubeEventHandler();
+export const myCube = new Cube();
+
+window.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    cubeEvent.initializeEventListeners();
+    myCube.generateCubeHTML();
+    
+    const mixButton = document.querySelector("#mixButton");
+    mixButton.addEventListener("click", () => myCube.mixCube());
+
+    const resetCube = document.querySelector("#resetCube");
+    resetCube.addEventListener("click", () => myCube.generateCubeHTML());
+
+    const resetPos = document.querySelector("#resetPos");
+    resetPos.addEventListener("click", () => myCube.resetPos());
+  },
+  false
+);
