8b5c0243d7e249812fe7d1f0c62d3d8da7adc0e2
NaloK7 <129076162+NaloK7@users.noreply.github.com>
2024-12-11 17:48:29 +0100

    fix: import path

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
-import { Cube } from "./rubiks/src/Cube.js";
-import { CubeEventHandler } from "./rubiks/src/CubeEventHandler.js";
+import { Cube } from "../src/Cube.js";
+import { CubeEventHandler } from "../src/CubeEventHandler.js";
 
 export const cubeEvent = new CubeEventHandler();
 export const myCube = new Cube();
